a48c19dbf8241a19410c385e1d63806d272fc998
Shu <stbook.syu@icloud.com>
2020-11-21 18:40:41 -0800

    add filter

--- src/App.js
+++ src/App.js
@@ -1,15 +1,47 @@
 import React from "react";
-
-function App() {
-  return (
-    <div className="grod-container">
-      <header>
-        <a href="/">Reaact Shopping Cart</a>
-      </header>
-      <main>Product List</main>
-      <footer>All right is reserved.</footer>
-    </div>
-  );
+import data from "./data.json";
+import Products from "./components/Products";
+import Filter from "./components/Filter";
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      products: data.products,
+      size: "",
+      sort: ""
+    };
+  }
+  sortProducts(event) {
+    console.log(event.target.value);
+  }
+  filterProducts(event) {
+    console.log(event.target.value);
+  }
+  render() {
+    return (
+      <div className="grid-container">
+        <header>
+          <a href="/">React Shopping Cart</a>
+        </header>
+        <main>
+          <div className="content">
+            <div className="main">
+              <Filter
+                count={this.state.products.length}
+                size={this.state.size}
+                sort={this.state.sort}
+                filterProducts={this.filterProducts}
+                sortProducts={this.sortProducts}
+              ></Filter>
+              <Products products={this.state.products}></Products>
+            </div>
+            <div className="sidebar">Cart Items</div>
+          </div>
+        </main>
+        <footer>All right is reserved.</footer>
+      </div>
+    );
+  }
 }
 
 export default App;
