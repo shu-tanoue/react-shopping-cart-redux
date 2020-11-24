fc295d123dce64fafd686bccc63462638fcdfdd6
Shu <stbook.syu@icloud.com>
2020-11-24 11:59:35 -0800

    cart area added

--- src/components/Products.js
+++ src/components/Products.js
@@ -14,7 +14,12 @@ export default class Products extends Component {
                 </a>
                 <div className="product-price">
                   <div>{formatCurrency(product.price)}</div>
-                  <button className="button primary">Add to Cart</button>
+                  <button
+                    onClick={() => this.props.addToCart(product)}
+                    className="button primary"
+                  >
+                    Add To Cart
+                  </button>
                 </div>
               </div>
             </li>
